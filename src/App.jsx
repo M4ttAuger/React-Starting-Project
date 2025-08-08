a6b71dc7571bdce1fa32781869a3f6b1135c2bf9
M4ttAuger <fanny.93.mr@gmail.com>
2025-08-08 02:54:04 +0200

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import componentsImg from "./assets/components.png";
+import Header from "./components/Header/Header.jsx";
+import CoreConcepts from "./components/CoreConcepts.jsx";
+import Examples from "./components/Examples.jsx";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <CoreConcepts />
+        <Examples />
+      </main>
+    </>
+  );
+}
+
+export default App;
